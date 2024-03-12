819d60e369ba123ecb1c21370b21525601040887
oldlonecoder <lussier.serge@gmail.com>
2024-03-12 08:54:35 -0400

    (fr) Ajouter les fichers source de tests

---
+++ Test.cxx
@@ -0,0 +1,19 @@
+//
+// Created by oldlonecoder on 24-03-12.
+//
+
+#include "Test.h"
+
+namespace lex
+{
+} // lex
+
+
+
+
+auto main(int argc, char **argv) -> int
+{
+    std::cout << "Duh! Refaire les tests!" << std::endl;
+    return 1;
+}
+
---
+++ Test.h
@@ -0,0 +1,34 @@
+//
+// Created by oldlonecoder on 24-03-12.
+//
+
+/******************************************************************************************
+ *   Copyright (C) /.../2024 by Serge Lussier                                             *
+ *   lussier.serge@gmail.com                                                              *
+ *                                                                                        *
+ *   Unless otherwise specified, all code in this project is written                      *
+ *   by the author (Serge Lussier)                                                        *
+ *   ----------------------------------------------------------------------------------   *
+ *   Copyrights from authors other than Serge Lussier also apply here                     *
+ *   Open source FREE licences also apply to the code from the same author (Serge Lussier)*
+ *   ----------------------------------------------------------------------------------   *
+ *****************************************************************************************/
+
+#pragma once
+
+#include <Lexer/Lexer.h>
+
+//#ifndef LEXER_TEST_H
+//#define LEXER_TEST_H
+
+namespace lex
+{
+
+class Test
+{
+
+};
+
+} // lex
+
+//#endif //LEXER_TEST_H
